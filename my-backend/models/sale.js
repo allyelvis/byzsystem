9449e84497b549662b21959be76d489557a7b0bb
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-07-27 21:32:33 +0000

    Update backend

---
+++ my-backend/models/sale.js
@@ -0,0 +1,21 @@
+export default (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    productId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Products',
+        key: 'id'
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    totalPrice: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    }
+  });
+
+  return Sale;
+};
